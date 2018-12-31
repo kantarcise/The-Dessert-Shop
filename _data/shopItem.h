61eee7ced46b22620a3f666f034d71bf8b9bb2e0
Sezai Burak Kantarcı <45287996+kantarcise@users.noreply.github.com>
2018-12-31 20:14:01 +0300

    Create shopItem.h

---
+++ _data/shopItem.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cString>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class shopItem {
+  public:	
+	int number, price;
+	string name;
+	void print();
+	int getCost(int,int);
+};
+
+int shopItem:getcost(int weight, int price_per_kg)
